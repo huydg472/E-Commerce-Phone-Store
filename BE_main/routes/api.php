60625fbc63ed75d8de5b872cd2d53496e15b6f16
datnguyen098 <hongdat098789@gmail.com>
2026-05-20 09:35:18 +0700

    Update api.php
    commit: testAPI

--- BE_main/routes/api.php
+++ BE_main/routes/api.php
@@ -1,10 +1,153 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CartItemController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductSpecificationController;
+use App\Http\Controllers\ProductVariantController;
+use App\Http\Controllers\ProductVariantImageController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ShippingAddressController;
+use App\Http\Controllers\StockLogController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('brand', BrandController::class)->only([
+    'index', //ok
+    'store', //ok
+    'show', //ok
+    'update', //ok
+    'destroy' //ok
+]);
+
+Route::apiResource('cart', CartController::class)->only([
+    'index', //ok
+    'store', //ok
+    'show', //ok
+    'update', //ok
+    'destroy' //ok
+]);
+
+
+Route::apiResource('cartitem', CartItemController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('category', CategoryController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('order', OrderController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('orderitem', OrderItemController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('payment', PaymentController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('permission', PermissionController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('product', ProductController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('productspecification', ProductSpecificationController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('productvariant', ProductVariantController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('productvariantimage', ProductVariantImageController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('role', RoleController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('shippingaddress', ShippingAddressController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('stocklog', StockLogController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
+
+Route::apiResource('user', UserController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
